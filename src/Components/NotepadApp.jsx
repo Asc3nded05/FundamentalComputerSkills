c3849fe59b99ca89ee646189af79734c709ece50
Riley Van Heukelum <rileyvanheukelum@gmail.com>
2026-02-05 09:43:33 -0500

    Fixed issue where closed apps could still be interacted with

--- src/Components/NotepadApp.jsx
+++ src/Components/NotepadApp.jsx
@@ -1,7 +1,7 @@
 function NotepadApp() {
     return (
         <div>
-            <textarea style={{position:"fixed", width:"100%", height: "100%", resize: "none", pointerEvents: "auto"}}>
+            <textarea style={{position:"fixed", width:"100%", height: "100%", resize: "none"}}>
                 Text here
             </textarea>
         </div>
