d433328cd8aed1d0ce5bc8fc40960ee3c2e45a97
ashg555 <ashleigh.gossman26@houghton.edu>
2026-02-05 09:26:21 -0500

    Made textarea in notepad 100% of app window

--- src/Components/NotepadApp.jsx
+++ src/Components/NotepadApp.jsx
@@ -1,7 +1,7 @@
 function NotepadApp() {
     return (
         <div>
-            <textarea style={{position:"absolute", width:"100%", height: "100%", resize: "none", pointerEvents: "auto"}}>
+            <textarea style={{position:"fixed", width:"100%", height: "100%", resize: "none", pointerEvents: "auto"}}>
                 Text here
             </textarea>
         </div>
