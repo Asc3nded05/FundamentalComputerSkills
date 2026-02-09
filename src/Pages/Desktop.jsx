f04b057573c03685f1d43aea56417b61a764a6de
aidanschooley <schooleyaidan7@gmail.com>
2026-02-09 14:27:44 -0500

    Update Desktop.jsx

--- src/Pages/Desktop.jsx
+++ src/Pages/Desktop.jsx
@@ -1,12 +1,12 @@
 import { Responsive, WidthProvider } from "react-grid-layout";
 import { useState } from 'react';
 
-import AppIcon from '../components/AppIcon.jsx'
-import Clock from '../components/Clock.jsx'
-import SideBar from '../components/SideBar.jsx'
-import AppWindow from '../components/AppWindow.jsx';
-import StartButton from "../components/StartButton.jsx";
-import StartMenu from "../components/StartMenu.jsx";
+import AppIcon from '../Components/AppIcon.jsx'
+import Clock from '../Components/Clock.jsx'
+import SideBar from '../Components/SideBar.jsx'
+import AppWindow from '../Components/AppWindow.jsx';
+import StartButton from "../Components/StartButton.jsx";
+import StartMenu from "../Components/StartMenu.jsx";
 
 import { APP_REGISTRY } from '../utils/apps.js';
 
