a53dbba8343dfdff0591d83eab53722cb40d1c18
SarahWinne <sarah.winne27@houghton.edu>
2026-02-20 14:53:41 -0500

    Added model file data

--- src/pages/Files.jsx
+++ src/pages/Files.jsx
@@ -5,39 +5,112 @@ import { Willow } from "@svar-ui/react-filemanager";
 
 const rawdata = [
   {
-    id: "/Code",
+    id: "/Desktop",
     size: 4096,
     date: new Date(2023, 11, 2, 17, 25),
     type: "folder",
   },
   {
-    id: "/Music",
+    id: "/Documents",
     size: 4096,
     date: new Date(2023, 11, 1, 14, 45),
     type: "folder",
   },
+    {
+      id: "/Documents/Folder",
+      size: 4096,
+      date: new Date(2025, 11, 1, 14, 45),
+      type: "folder",
+    },
+  {
+    id: "/Downloads",
+    size: 4096,
+    date: new Date(2025, 11, 1, 14, 45),
+    type: "folder",
+  },
+  {
+    id: "/Music",
+    size: 4096,
+    date: new Date(2023, 11, 2, 17, 25),
+    type: "folder",
+  },
+  {
+    id: "/Pictures",
+    size: 4096,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "folder",
+  },
+    {
+      id: "/Pictures/Cat Photos",
+      size: 4096,
+      date: new Date(2025, 11, 1, 14, 45),
+      type: "folder",
+    },
+  {
+    id: "/Videos",
+    size: 4096,
+    date: new Date(2025, 11, 1, 14, 45),
+    type: "folder",
+  },
 
   {
-    id: "/Info.txt",
-    size: 1000,
-    date: new Date(2023, 10, 30, 6, 13),
-    type: "file",
-  },
-  {
-    //parent: "/Code/Datepicker",
-    id: "/Code/Datepicker/Year.jsx",
-    size: 1595,
-    date: new Date(2023, 11, 7, 15, 23),
-    type: "file",
-  },
-  {
-    id: "/Pictures/162822515312968813.png",
+    id: "/Documents/Boring Document",
     size: 510885,
     date: new Date(2023, 11, 1, 14, 45),
     type: "file",
   },
+  {
+    id: "/Documents/Example Text File",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Documents/Folder/Secret Document",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Downloads/Photo.png",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Downloads/Document.txt",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Music/BirdsChirping.mp3",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Pictures/Cat Photos/CatPic.png",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Pictures/Photo.png",
+    size: 510885,
+    date: new Date(2023, 11, 1, 14, 45),
+    type: "file",
+  },
+  {
+    id: "/Videos/RickRoll.mp4",
+    size: 1595,
+    date: new Date(2023, 11, 7, 15, 23),
+    type: "file",
+  },
 ];
 
+// Refer to for help with file manager:
+// https://docs.svar.dev/react/filemanager/api/overview/api_overview
 function Files() {
   return <>
     <Willow>
