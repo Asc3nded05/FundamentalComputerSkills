807afdf6c54787d0efd9c8634056766ffef9f1ac
aidanschooley <schooleyaidan7@gmail.com>
2026-03-18 16:47:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/FundamentalComputerSkills/',
-  base: './',
+  base: '/FundamentalComputerSkills/',
+  // base: './',
   server: {
     proxy: {
       '/api': {
