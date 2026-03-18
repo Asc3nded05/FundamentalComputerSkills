8041616e0141a8384d41927e6b25895dfaa1db05
aidanschooley <schooleyaidan7@gmail.com>
2026-03-18 17:23:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://4.172.249.10:3000',
+        target: 'https://4.172.249.10:3000',
         changeOrigin: true,
         secure: false,
         ws: true,
