e7f0e8caa265127677d8ac8fe2c7e331458c6b14
aidanschooley <schooleyaidan7@gmail.com>
2026-03-18 23:56:33 -0400

    Update Personalization.jsx

--- src/components/settings/Personalization.jsx
+++ src/components/settings/Personalization.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const Personalization = () => {
     const [subPage, setSubPage] = useState('main'); // 'main' or 'background'
     const [backgroundImage, setBackgroundImage] = useState(
-        localStorage.getItem('backgroundImage') || '../assets/background-image.jpg'
+        localStorage.getItem('backgroundImage') || '../FundamentalComputerSkills/assets/background-image.jpg'
     );
 
     const handleBackgroundChange = (newImage) => {
