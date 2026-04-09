999bd2187ba0f90b2c42a9547ace6a3d3099eccd
Riley Van Heukelum <rileyvanheukelum@gmail.com>
2026-04-09 09:14:47 -0400

    Updated getAppsByLessonId API SQL query to retrieve new table columns.

--- backend/services/lessonService.js
+++ backend/services/lessonService.js
@@ -60,7 +60,7 @@ export async function getAppsByLessonId(lessonId) {
         connection = await initOracle();
 
         const result = await connection.execute(
-            `SELECT a.appId, a.registryId, a.appIcon
+            `SELECT a.appId, a.registryId, a.appIcon, a.cpuMin, a.cpuMax, a.memMin, a.memMax, a.diskMin, a.diskMax, a.netMin, a.netMax
             FROM ADMIN.App a
             INNER JOIN ADMIN.LessonApp la ON a.appId = la.appId
             WHERE la.lessonId = :lessonId
