d18b32edf80c371110dcca65b62f0adb1792102e
ashg555 <ashleigh.gossman26@houghton.edu>
2026-04-28 09:32:11 -0400

    Fixed unresponsive apps in Unresponsive App lesson

--- src/components/Unresponsive.jsx
+++ src/components/Unresponsive.jsx
@@ -3,7 +3,6 @@ import "../css/Unresponsive.css";
 function Unresponsive() {
     return (
         <div className="unresponsive-container">
-            <h2>Application is Unresponsive</h2>
         </div>
     );
 }
